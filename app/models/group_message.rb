5667b6a39e5b54910ce9d04e82594e6797f0e03f
revyll ogandaga <revyll44@gmail.com>
2025-06-26 23:40:45 +0300

    styling the input field for group like whatsapp

--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -3,4 +3,14 @@ class GroupMessage < ApplicationRecord
   belongs_to :user
   # Pour gérer les fichiers attachés (optionnel, si tu veux ActiveStorage)
   has_one_attached :file # Permet l'upload de tout type de fichier
+
+  validate :content_or_file_present
+
+  private
+
+  def content_or_file_present
+    if content.blank? && !file.attached?
+      errors.add(:base, "Le message ne peut pas être vide.")
+    end
+  end
 end
